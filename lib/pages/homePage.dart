7122b7981e1c698b9b0feb2d9e92e68c1c0f096c
giantpro1 <shakiruolawale597@gmail.com>
2024-02-15 22:46:52 +0100

    homepage ....

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -11,10 +11,57 @@ class HomePage extends StatelessWidget {
         children: [
           const HomeAppBar(),
           Container(
+            height: 500,
             padding: const EdgeInsets.only(top: 15),
             decoration: const BoxDecoration(
               color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
             ),
+            child: Column(children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 5),
+                      height: 50,
+                      width: 300,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search Here .....",
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.camera_alt,
+                      size: 27,
+                      color: Color(0xFF4C53A5),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                child: const Text(
+                  "Category",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ]),
           )
         ],
       ),
